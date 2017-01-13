0eb865e43340d70e5a4cceb497c5718d097915ef
Ihor <fesianov.i@gmail.com>
2017-01-13 22:31:36 +0200

    version after formatting code using PyCharm keys

--- src/homework_1/h1.5.2.py
+++ src/homework_1/h1.5.2.py
@@ -3,15 +3,17 @@ import math
 
 num = input()
 
+
 def factorial(num):
     mult = 1
     for multer in range(num):
         mult *= multer + 1
     return mult
 
+
 def factorial_stirl(num):
-    return int((2*math.pi*num)**0.5*(num/math.exp(1))**num)
+    return int((2 * math.pi * num) ** 0.5 * (num / math.exp(1)) ** num)
 
-print factorial(num)
-print factorial_stirl(num)
 
+print (factorial(num))
+print (factorial_stirl(num))
