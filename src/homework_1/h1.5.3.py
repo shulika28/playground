aeff58eaefae3be4e42db1badcdf2569dac97992
Ihor <fesianov.i@gmail.com>
2017-01-13 22:29:08 +0200

    version with filter method

--- src/homework_1/h1.5.3.py
+++ src/homework_1/h1.5.3.py
@@ -1,13 +1,11 @@
 __author__ = 'Ihor'
-import math
 
 num = input()
 
-def counter_eights(num):
-    count = 0
-    for el in str(num):
-        if el == '8':
-            count +=1
-    return count
 
-print counter_eights(num)
+def counter_eights(num):
+    return len(list(filter(lambda x: x == '8', str(num))))
+
+
+print(counter_eights(num))
+
